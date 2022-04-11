53d6bc186b7dcfd804adbd28b9bc13a04e467bef
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2022-04-11 14:56:25 +0200

    Made calls to server for data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
+    <Header
+      @toggleAddTask="toggleAddTask"
+      :taskFormShown="showTaskForm"
+      title="Task Tracker"
+    />
+    <div v-show="showTaskForm">
+      <TaskForm @addTask="addTask" />
+    </div>
     <Tasks
       @toggleReminder="toggleReminder"
       @deleteTask="deleteTask"
@@ -12,57 +19,72 @@
 <script>
 import Header from "./components/Header.vue";
 import Tasks from "./components/Tasks.vue";
+import TaskForm from "./components/TaskForm.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     Tasks,
+    TaskForm,
   },
   data() {
     return {
       tasks: [],
+      showTaskForm: false,
     };
   },
   methods: {
-    deleteTask(taskId) {
-      if (confirm("Are you sure you want to DELETE this task?")) {
-        this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    async addTask(taskData) {
+      const response = await fetch("api/tasks", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(taskData),
+      });
+
+      const task = await response.json();
+
+      this.tasks = [...this.tasks, task];
+    },
+    async getTasks() {
+      const response = await fetch("api/tasks");
+      const tasks = await response.json();
+
+      return tasks;
+    },
+    async getTask(id) {
+      const response = await fetch(`api/tasks/${id}`);
+      const task = await response.json();
+
+      return task;
+    },
+    async deleteTask(id) {
+      if (this.deleteConfirmed()) {
+        const response = await fetch(`api/tasks/${id}`, { method: "DELETE" });
+
+        response.status === 200 ? this.removeDeletedTask(id) : this.alertUser();
       }
+    },
+    removeDeletedTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    deleteConfirmed() {
+      return confirm("Are you sure you want to DELETE this task?");
+    },
+    alertUser() {
+      alert("Error deleting task");
     },
     toggleReminder(taskId) {
       this.tasks = this.tasks.map((task) =>
         task.id === taskId ? { ...task, reminder: !task.reminder } : task
       );
     },
+    toggleAddTask() {
+      this.showTaskForm = !this.showTaskForm;
+    },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: "Doctor's Appointment",
-        day: "February 23rd at 12:00pm",
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: "Meeting at School",
-        day: "February 21st at 9:30am",
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: "Dev Learning Session",
-        day: "March 29th at 2:00pm",
-        reminder: false,
-      },
-      {
-        id: 4,
-        text: "Magic Box: Demo Session",
-        day: "March 30th at 12:00pm",
-        reminder: true,
-      },
-    ];
+  async created() {
+    this.tasks = await this.getTasks();
   },
 };
 </script>
