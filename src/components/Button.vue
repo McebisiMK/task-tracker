4ac46ae90e7be60e46432f978173ef267aa3e3ca
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2022-04-11 07:32:29 +0200

    WIP: Added button component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="onAddTask()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onAddTask() {
+      console.log("Added new task");
+    },
+  },
+};
+</script>
