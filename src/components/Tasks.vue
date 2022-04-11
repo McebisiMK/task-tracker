ace0c1b229eab24c32a53f6347ac44ea04ce07c3
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2022-04-11 07:31:07 +0200

    Added list of tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task
+      @toggleReminder="toggleReminder(task.id)"
+      @deleteTask="deleteTask(task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  methods: {
+    deleteTask(taskId) {
+      this.$emit("deleteTask", taskId);
+    },
+    toggleReminder(taskId) {
+      this.$emit("toggleReminder", taskId);
+    },
+  },
+  emits: ["deleteTask", "toggleReminder"],
+};
+</script>
